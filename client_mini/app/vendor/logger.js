2cf970e2f77e89680df3ab9325e1ce2a380768f5
Vitaly <vitaly.tarasenko@crossover.com>
2019-04-02 10:37:30 +0300

    add github link

--- client_mini/app/vendor/logger.js
+++ client_mini/app/vendor/logger.js
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2019 Vitaly Tarasenko
+Copyright (c) 2019 Vitaly Tarasenko, https://github.com/tarvit
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
